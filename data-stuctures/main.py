23b26ab6d717bd160b75d3a8c360ec59a439eb12
Alessio <alessioalessio63@yahoo.com>
2022-09-12 20:44:03 +0300

    data structures

---
+++ data-stuctures/main.py
@@ -0,0 +1,38 @@
+# # # # # my_tuple = (10, 20, 30, 40, 50)
+# # # # # # #           0   1   2   3   4
+# # # # # # #          -5  -4  -3  -2  -1
+# # # # # # my_list = list()
+# # # # # # print(my_list)
+# # # # # my_list = list(my_tuple)
+# # # # # print('my_list', my_list, type(my_list))
+# # # # # my_list.append(60)
+# # # # # my_list.append(70)
+# # # # # my_tuple = tuple(my_list)
+# # # # # print('my_tuple', my_tuple, type(my_tuple), id(my_tuple))
+# # # #
+# # # # # my_empty_dict = {}
+# # # # # print('my_empty_dict', my_empty_dict, type(my_empty_dict))
+# # # #
+# # # # my_dict = {"a": 2, "b": "dan", "c": [1, 2, 3]}
+# # #
+# # # # print('my_dict', my_dict)
+# # # my_data = {
+# # #     0: 'ana'
+# # #     1: 'are'
+# # #     2: 'mere'
+# # # }
+# # # print(my_data[0])
+# my_dict = {
+#      'a': 10
+# }
+#
+# del my_dict['a']
+#
+#
+# #.keys() / .items() / .values()
+# my_dict = {
+#     'a': 1,
+#     'b': 2,
+#     'c': 3
+# }
+# print(my_dict)
