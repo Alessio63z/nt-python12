17b855d07f3dd005d0c828c60275afc821af9bca
Alessio <alessioalessio63@yahoo.com>
2022-09-17 19:51:01 +0300

    data structures - tema

---
+++ data stuctures - tema/main.py
@@ -0,0 +1,12 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+#          0  1  2  3  4  5  6  7   8  9
+my_list.sort()
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[1::2])
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[0::2])
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(my_list[2::3])
